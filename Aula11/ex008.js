dc18c63afc5d39231a9f0a9418572e3e0726e2f1
Johnny Andrade <94999175+Johnny-Andrade@users.noreply.github.com>
2026-03-23 15:16:21 -0300

    Aulas 10 e 11 concluídas

---
+++ Aula11/ex008.js
@@ -0,0 +1,8 @@
+var vel = 60.5
+console.log(`A velocidade do seu carro é ${vel}km/h.`)
+
+if(vel > 80){ //Condição Simples, não possui else
+    console.log(`Você ultrapassou a velocidade permitida, multado!`)
+}
+
+console.log('Dirija sempre usando cinto de segurança!')
---
+++ Aula11/ex009.js
@@ -0,0 +1,7 @@
+var pais = 'EUA'
+console.log(`Você vive em ${pais}.`)
+if(pais == 'Brasil' || pais == 'BR'){
+    console.log('Aí sim! Brasileiro.')
+} else{
+    console.log('SOME DAQUI SEU GRINGO!')
+}
